b32020b9d0560d54e674845127eeeeaef57c92d0
mbanel <mariabanel@gmail.com>
2018-08-11 12:06:54 +0200

    Add files via upload

--- slide.js
+++ slide.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
 	alert($(window).width());
-	if($(window).width() > 800) {
 		$('.cont>p').hide();
 		$('.cont>h4').click(function() {
 			$(this).next().slideToggle('fast');
 			$(this).toggleClass('active');
 		});
-	}
 });
